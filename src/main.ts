cff205419bc88ba3bc2be2e808ca6274add76be5
Александр <bdwsk87dev@gmail.com>
2022-04-29 04:43:51 +0300

    front_1

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import {NestFactory} from "@nestjs/core";
 import {AppModule} from "./app.module";
+import { join } from 'path';
 
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function start(){
     const PORT = process.env.PORT || 5000;
     const app = await NestFactory.create<NestExpressApplication>(AppModule)
+
+    app.useStaticAssets(join(__dirname, '..', 'public'));
+    app.setBaseViewsDir(join(__dirname, '..', 'views'));
+    app.setViewEngine('hbs');
+
     await app.listen(PORT, ()=> console.log(`Сервер запущен на порту : ${PORT}`))
 }
 
